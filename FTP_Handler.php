93feea9e828e3e6e6e5757475137e8f714ff8b76
Matthew Watts <mwatts@ocenture.com>
2012-11-18 12:23:55 -0500

    Updated FTP Handler Class
    Added member array with connection details and resources from
    connection attempt.

--- FTP_Handler.php
+++ FTP_Handler.php
@@ -1,14 +1,26 @@
 <?php
 
-class FTP_Handler { 
-	
+class FTP_Handler {
+
+	// The connection settings that we will be using.
 	protected $connectionSettings;
-	
-	public function __construct() { 
-		
+
+	public function __construct( $hostname, $username, $password, $workingDirectory ) {
+
+		$this->connectionSettings = array
+			
+		(
+				"hostname" 				=> $hostname, 
+				"username" 				=> $username, 
+				"password" 				=> $password,
+				"workingDirectory" 		=> $workingDirectory,
+				"stream" 				=> ftp_connect($hostname),
+				"result" 				=> ftp_login(ftp_connect($hostname), $username, $password),
+		);
+
 	}
-	
-	public function __destruct(){ 
-		
+
+	public function __destruct(){
+
 	}
 }
